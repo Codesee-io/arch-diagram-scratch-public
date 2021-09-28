4e03c4cd1ea7a2159a9e71e9aec5e0ec772f5cbf
Grayson Hay <grayson@codesee.io>
2021-09-28 11:54:28 -0700

    next

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 console.log("This is the song that never ends");
 console.log("It goes on and on my friend");
 console.log("It started a long time ago");
+console.log("I don't remember the rest of this song, but...");
